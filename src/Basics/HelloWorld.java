3ac113f7d3c25c01b10819fd2743775829d3d401
Yordanos <yordanos80@gmail.com>
2017-12-05 16:31:32 +0100

    This is my second time to add to repository

--- src/Basics/HelloWorld.java
+++ src/Basics/HelloWorld.java
@@ -1,5 +1,18 @@
 package Basics;
 
 public class HelloWorld {
+	public static void main(String[]args)
+	{
+		int i = 10;
+		
+		for (int m=0;m<i;m++)
+		{
+			for (int s=0;s<i;s++)
+			{	
+		      System.out.print(s);
+			}
+		      System.out.println();
+		}     
+	}
 
 }
